76002276e1518106e6da2738b6e001d0ab46cb87
dgiraldos <danielgiral2@gmail.com>
2026-06-13 14:27:50 +0200

    cambios

--- pm_app/urls.py
+++ pm_app/urls.py
@@ -7,4 +7,9 @@ app_name = 'pm_app'
 urlpatterns = [
     # Página de inicio
     path('', views.index, name='index'),
+    # Página de visualización de comidas
+    path("comidas/<int:comida_id>/", views.comida, name="comida"),
+    path('comidas/', views.comidas, name='comidas'),
+    # Página de visualización de nuestro planificador
+    path('planificacion', views.planificacion, name='planificacion'),
 ]
--- pm_app/views.py
+++ pm_app/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Comida, Planificacion
 # Create your views here.
 
 def index(request):
     """La pagina de inicio para pm_app"""
+    planificaciones = Planificacion.objects.select_related("comida").order_by("dia", "tipo_comida")
+    context = {'planificaciones': planificaciones}
     return render(request, 'pm_app/index.html')
+
+def comidas(request):
+    """Muestra todas las comidas"""
+    comidas = Comida.objects.order_by('nombre')
+    context = {'comidas': comidas}
+    return render(request, 'pm_app/comidas.html', context)
+
+def comida (request):
+    """Detalle de una comida"""
+    comida = get_object_or_404(Comida, id=comida_id)
+    context = {'comida': comida}
+    return render(request, 'pm_app/comida.hmtl', context)
+
+def planificacion(request):
+    """Muestra nuestra planificacion"""
+    planificaciones = Planificacion.objects.select_related('comida').order_by('dia', 'tipo_comida')
+    context = {'planificaciones': planificaciones}
+    return render(request, 'pm_app/planificacion.html', context)
